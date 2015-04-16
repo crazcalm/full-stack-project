81b7b37d4bdaa81762b5995a30de73e8aeb0ef26
Crazcalm <crazcalm@gmail.com>
2015-04-16 00:50:19 -0400

    I am dropping the manager extenstion so that I can use my custom commandline configuration app runner script

--- app/run.py
+++ app/run.py
@@ -1,28 +1,59 @@
-from flask.ext.script import Manager
+import argparse
 from app import app
 from configuration import ProductionConfig
 from configuration import StagingConfig
 from configuration import DevelopmentConfig
 
 
-def set_config(name):
+# Constants
+DESCRIPTION = "A commandline interface for selecting the configuration file."
+EPILOG = "See configuration.py for specific configuration details."
+DEVELOPMENT = "Development configuration"
+STAGING = "Staging configuration"
+PRODUCTION = "Production configuration"
+
+
+def set_config(args):
     """
     A function to select a config option.
     """
-    if name == "dev":
+    if args.dev:
+        print("dev")
         app.config.from_object(DevelopmentConfig)
-    elif name == "stage":
+    elif args.stage:
+        print("stage")
         app.config.from_object(StagingConfig)
-    elif name == "prod":
+    elif args.prod:
+        print("prod")
         app.config.from_object(ProductionConfig)
     else:
-        print("Not a valid config option!")
-        return None
+        print("The default configuration is DevelopmentConfig.\n")
+        app.config.from_object(DevelopmentConfig)
     return app
 
 
 if __name__ == "__main__":
-    configured_app = set_config("stage")
-    print(configured_app.config)
-#manager = Manager(app)
-#manager.run()
+
+    # Command line logic
+    parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
+
+    # This prevents users from passing multiple options at once
+    group1 = parser.add_mutually_exclusive_group()
+
+    # Adds the commandline options
+    group1.add_argument("-dev", "--development", dest="dev",
+                        action="store_true", help=DEVELOPMENT)
+
+    group1.add_argument("-stage", "--staging", dest="stage",
+                        action="store_true", help=STAGING)
+
+    group1.add_argument("-prod", "--production", dest="prod",
+                        action="store_true", help=PRODUCTION)
+
+    # Dictionary with the commandline inputs
+    args = parser.parse_args()
+
+    configured_app = set_config(args)
+
+    if configured_app:
+        configured_app.run('localhost', port=5000)
