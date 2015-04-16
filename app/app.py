46b3b4321e7b538c8138cdbf24a5f9459ea7129a
Crazcalm <crazcalm@gmail.com>
2015-04-16 01:07:30 -0400

    removing gunicorn from my project

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-from flask.ext.script import Manager
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,10 +6,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World"
-
-manager = Manager(app)
-app.config['DEBUG'] = True  # Ensure debugger will load
-
-
-if __name__ == "__main__":
-    manager.run()
