7fed389104796594077dc1f72975371b542883d5
Crazcalm <crazcalm@gmail.com>
2015-04-18 02:17:32 -0400

    Not tested; Not used; need to finish

--- Provision/nginx/setup_nginx.py
+++ Provision/nginx/setup_nginx.py
@@ -20,13 +20,38 @@ import os
 NGINX = {
     "defualt_file": "/etc/nginx/sites-enabled/default",
     "app_config": "/vagrant/Provision/nginx/flask_project",
-    "testing" : False
-    }
+    "testing": False
+}
+
+
+def create_cmds(config):
+    """
+    This function creates the commands needed for the
+    ngnix setup.
+    """
+    cmd1 = "rm /etc/nginx/sites-enabled/default"
+    cmd2 = (
+        "echo '$(cat {})' > "
+        "/etc/nginx/sites-available/flask_project"
+    ).format(congig.app_config))
+
+    cmd3 = ("ln -s /etc/nginx/sites-available/flask_project "
+        "/etc/nginx/sites-enabled/flask_project")
+    cmd4 = "/etc/init.d/nginx restart"
+
+    results = [cmd1, cmd2, cmd3, cmd4]
+    return results
 
 def setup_nginx(config):
-    cmd1 = "rm /etc/nginx/sites-enabled/default"
-    cmd2 = "echo '$(cat {})' > /etc/nginx/sites-available/flask_project".format(congig.app_config)
+    commands = create_cmds(config)
+    for cmd in commands:
+        subprocess.call(cmd, shell=True)
 
 def main(config):
     if os.exist(config.defualt_file):
         setup_nginx(config)
+        print("The setup process has finished")
+
+    else:
+        print("Nginx has already been setup.")
+        subprocess.call("/etc/init.d/nginx restart", shell = True)
