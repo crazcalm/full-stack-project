42e8d38dbe87df97ad074d1ddb7696779b3eb42e
Crazcalm <crazcalm@gmail.com>
2015-04-23 18:56:58 -0400

    I need to get going to class. I will finish this later

--- app/tests/test_run.py
+++ app/tests/test_run.py
@@ -7,10 +7,38 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from run import set_config
 
+"""
+To test this, I need to both mock the app.config.from_object call and
+check the capture the stdout in a buffer so that I can check the text.
 
-class TestRun(unittest.TestCase):
+OR,
+
+I can create a fake app class here and do the buffer stuff tonight...
+
+"""
+
+
+class TestConfig:
+    def __init__(self):
+        self.name = "Used me for testing only!"
+
+    def from_object(self, object):
+        # I should check the class type and return an answer here!!!!
+        pass
+
+
+class TestApp:
+    def __init__(self):
+        self.dev = False
+        self.stage = False
+        self.prod = False
+        self.config = TestConfig()
+
+
+class TestRun(unittest.TestCase): # Still need to add the logig for the tests
     def setUp(self):
         self.func = set_config
+        self.test_object = TestApp()
         self.options = {
             "dev": "dev",
             "prod": "prod",
