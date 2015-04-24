e17f48a71dfce9d5969eca06bf1a8bcc55ece7fa
Crazcalm <crazcalm@gmail.com>
2015-04-24 19:55:13 -0400

    I will try to call the source command from a bash script

--- Provision/python_env/setup_virtualenv.py
+++ Provision/python_env/setup_virtualenv.py
@@ -11,7 +11,8 @@ INSTRUCTIONS = {
     "bashrc_location": "~/.bashrc",
     "enable": "source ~/.bashrc",
     "install_ez_setup": "python /vagrant/Provision/python_env/ez_setup.py",
-    "install_pip": "easy_install pip"
+    "install_pip": "easy_install pip",
+    "bash_script": "/vagrant/Provision/python_env/setup_virtualenv.sh"
 }
 
 
@@ -43,6 +44,13 @@ def setup_virtualenv(config):
             shell=True
         )
 
+        subprocess.call(
+            "bash {}".format(
+                config["bash_script"]
+            ),
+            shell=True
+        )
+
 
 def main(config):
     if os.path.exists(config["check"]):
@@ -52,8 +60,8 @@ def main(config):
         print("Started the virtualenv setup process.")
         setup_virtualenv(config)
         print("The Virtualenv setup has been completed!")
-        print("To complete install run")
-        print("\n\nsource ~/.bashrc")
+        #print("To complete install run")
+        #print("\n\nsource ~/.bashrc")
 
 
 if __name__ == '__main__':
---
+++ Provision/python_env/start_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/.bashrc
+echo Virtualenv is Ready to Go!
