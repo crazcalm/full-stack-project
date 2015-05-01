786a23bd99fd65be99375bd339f3ff9ac40377df
Crazcalm <crazcalm@gmail.com>
2015-04-30 20:22:37 -0400

    I added another route to mess with the html5 boilerplate stuff. Also, the nginx default file was not deleted. I need to check that

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World"
+
+@app.route("/home")
+def home():
+    return render_template("index.html")
