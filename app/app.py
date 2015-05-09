2934f901c0e4b63e8f98a916be7efe0bde2542ee
Crazcalm <crazcalm@gmail.com>
2015-05-09 11:21:17 -0400

    Added a number of other route that all inherit from the base template

--- app/app.py
+++ app/app.py
@@ -12,8 +12,50 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """
+    Renders the hello world test page.
+    """
     return "Hello World"
 
-@app.route("/home")
+
+@app.route("/base")
 def home():
-    return render_template("index.html")
+    """
+    This method renders the base tempaplate.
+
+    :return: html page
+    """
+    return render_template("base.html")
+
+
+@app.route("/developer")
+def developer():
+    """
+    This method renders the develper page, which
+    is dedicated to introducing the "developer Marcus".
+
+    :return: html page
+    """
+    return render_template("developer.html")
+
+
+@app.route("/resume")
+def resume():
+    """
+    This method renders the resume page, which will
+    render an html version of my resume.
+
+    :return: html page
+    """
+    return render_template("resume.html")
+
+
+@app.route("/chinese")
+def chinese():
+    """
+    This method renders the chinese page, which will be dedicated
+    to giving me an outlet to host chinese related things.
+
+    :return: html page
+    """
+    return render_template("chinese.html")
