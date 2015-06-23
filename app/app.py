9dc321aa122e0931a78bcd059ca5490b6c51ed46
Crazcalm <Crazcalm@gmail.com>
2015-06-23 14:12:23 -0400

    404 and 500 page are now done

--- app/app.py
+++ app/app.py
@@ -73,3 +73,7 @@ def friends():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
